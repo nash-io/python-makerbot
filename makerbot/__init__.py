3e661a7768db79a2fa1db1232250c5306025218f
Fabio C. Canesin <fabio.canesin@gmail.com>
2019-12-06 20:28:02 +0100

    Fix GitLab path for pypi package

--- makerbot/__init__.py
+++ makerbot/__init__.py
@@ -3,7 +3,7 @@
 """Top-level package for makerbot."""
 
 __author__ = """Nash"""
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .core import main
 from .helpers import Order, OrderBookSeries, retry, get_config
